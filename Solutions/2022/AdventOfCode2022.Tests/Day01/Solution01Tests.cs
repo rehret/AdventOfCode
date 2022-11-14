593dbe70a4bb7df4c1eb345ac6c921b2e6f5cc65
Randy Ehret <randyehret@gmail.com>
2022-11-14 07:41:26 -0600

    Add test project for 2022

---
+++ Solutions/2022/AdventOfCode2022.Tests/Day01/Solution01Tests.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode2022.Tests.Day01;
+
+public class Solution01Tests
+{
+    [Fact]
+    public async Task SolveAsync_WithSampleInput_ProducesSampleOutput()
+    {
+        // Arrange
+
+
+        // Act
+
+        // Assert
+    }
+}
