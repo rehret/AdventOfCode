a335a80d2ee21c56f8f64b2951a75c106d47f325
Randy Ehret <randyehret@gmail.com>
2022-11-15 15:21:21 -0600

    Cache referenced assemblies in AssemblyHelpers.GetReferencedAssemblies so they don't have to be grabbed more than once

--- AdventOfCodeRunner/Helpers/AssemblyHelpers.cs
+++ AdventOfCodeRunner/Helpers/AssemblyHelpers.cs
@@ -7,13 +7,20 @@ internal static class AssemblyHelpers
 {
     private static readonly Regex ReferencedAssemblyPattern = new(@"AdventOfCode.*\.dll", RegexOptions.Compiled);
 
+    private static Assembly[]? _referencedAssemblies;
+    private static readonly object ReferencedAssembliesLock = new();
+
     public static Assembly[] GetReferencedAssemblies(bool includeCurrentAssembly = true)
     {
         var thisAssembly = Assembly.GetExecutingAssembly();
-        return Directory.EnumerateFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll", SearchOption.AllDirectories)
-            .Where(filename => ReferencedAssemblyPattern.IsMatch(filename))
-            .Select(Assembly.LoadFrom)
-            .Where(assembly => includeCurrentAssembly || assembly != thisAssembly)
-            .ToArray();
+        lock (ReferencedAssembliesLock)
+        {
+            _referencedAssemblies ??= Directory
+                .EnumerateFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll", SearchOption.AllDirectories)
+                .Where(filename => ReferencedAssemblyPattern.IsMatch(filename))
+                .Select(Assembly.LoadFrom)
+                .ToArray();
+        }
+        return _referencedAssemblies.Where(assembly => includeCurrentAssembly || assembly != thisAssembly).ToArray();
     }
 }
