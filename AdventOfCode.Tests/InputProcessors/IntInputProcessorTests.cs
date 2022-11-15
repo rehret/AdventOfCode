151ebf0f71c2781265bbc881acdf524be53e3670
Randy Ehret <randyehret@gmail.com>
2022-11-15 08:12:06 -0600

    Add tests for input processors

---
+++ AdventOfCode.Tests/InputProcessors/IntInputProcessorTests.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode.Tests.InputProcessors;
+
+using AdventOfCode.InputProcessors;
+
+public class IntInputProcessorTests
+{
+    private readonly IntInputProcessor _inputProcessor;
+
+    public IntInputProcessorTests()
+    {
+        _inputProcessor = new IntInputProcessor();
+    }
+
+    [Theory]
+    [InlineData(1, "1")]
+    [InlineData(100, "100")]
+    [InlineData(0, "0")]
+    [InlineData(-1, "-1")]
+    [InlineData(-100, "-100")]
+    public void Process_GivenValidInput_CorrectlyParsesInt(int expected, string input)
+    {
+        // Act
+        var result = _inputProcessor.Process(new[] { input }).First();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
---
+++ AdventOfCode.Tests/InputProcessors/StringInputProcessorTests.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode.Tests.InputProcessors;
+
+using AdventOfCode.InputProcessors;
+
+public class StringInputProcessorTests
+{
+    private readonly StringInputProcessor _inputProcessor;
+
+    public StringInputProcessorTests()
+    {
+        _inputProcessor = new StringInputProcessor();
+    }
+
+    [Theory]
+    [InlineData("Test String", "Test String")]
+    [InlineData("", "")]
+    public void Process_GivenValidInput_ReturnsInputString(string expected, string input)
+    {
+        // Act
+        var result = _inputProcessor.Process(new[] { input }).First();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
