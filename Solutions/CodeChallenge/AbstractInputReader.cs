329478fa577a0d2ac74a713636fc5a924eb892d5
Randy Ehret <randyehret@gmail.com>
2022-11-17 13:18:42 -0600

    Small refactor in AbstractInputReader to use built-in functionality for trimming and discarding empty lines

--- Solutions/CodeChallenge/AbstractInputReader.cs
+++ Solutions/CodeChallenge/AbstractInputReader.cs
@@ -10,9 +10,7 @@ public abstract class AbstractInputReader<TChallengeSelection> : IInputReader<TC
         var filepath = GetInputFilePath(challengeSelection);
         using var streamReader = new StreamReader(filepath, Encoding.UTF8);
         return (await streamReader.ReadToEndAsync().ConfigureAwait(false))
-            .Split('\n')
-            .Where(line => !string.IsNullOrWhiteSpace(line))
-            .Select(line => line.Trim());
+            .Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
     }
 
     protected abstract string GetInputFilePath(TChallengeSelection challengeSelection);
