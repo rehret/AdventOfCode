9d7c266d47d7be08097f60aefd8c51d76187d04e
Randy Ehret <randyehret@gmail.com>
2022-11-18 16:22:37 -0600

    Remove unused configuration for Tom's Data Onion

--- Solutions/TomsDataOnion/CodeChallenge.TomsDataOnion/Configuration/TomsDataOnionConfiguration.cs
+++ Solutions/TomsDataOnion/CodeChallenge.TomsDataOnion/Configuration/TomsDataOnionConfiguration.cs
@@ -4,7 +4,5 @@ using CodeChallenge.Core.Configuration;
 
 internal class TomsDataOnionConfiguration : ICodeChallengeConfiguration
 {
-    public bool UseSimpleBase { get; set; } = false;
-
     public string OutputFileSuffix { get; set; } = "";
 }
