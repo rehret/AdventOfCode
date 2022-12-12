6576646c4ae2c3b66348eef2d6f32b6416900e95
Randy Ehret <randyehret@gmail.com>
2022-12-12 11:36:33 -0600

    Implement Dijktra's algorithm

---
+++ CodeChallenge.Core/Helpers/Math/Dijkstra.cs
@@ -0,0 +1,77 @@
+﻿namespace CodeChallenge.Core.Helpers.Math;
+
+public static class Dijkstra
+{
+    public record ShortestPathGraphResult<T>(
+        IReadOnlyDictionary<T, int> Distances,
+        IReadOnlyDictionary<T, T> PreviousNodes
+    );
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="graph"></param>
+    /// <param name="source"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <remarks>Implemented using pseudocode found at: https://en.wikipedia.org/wiki/Dijkstra's_algorithm#Using_a_priority_queue</remarks>
+    /// <returns></returns>
+    public static ShortestPathGraphResult<T> GetShortestPathGraph<T>(Graph<T> graph, T source)
+        where T : IEquatable<T?>
+    {
+        var dist = graph.GetVertices().ToDictionary(vertex => vertex, _ => int.MaxValue);
+        var prev = new Dictionary<T, T>();
+        var queue = new PriorityQueue<T, int>();
+
+        dist[source] = 0;
+        queue.Enqueue(source, 0);
+
+        while (queue.Count > 0)
+        {
+            var vertex = queue.Dequeue();
+            foreach (var (_, neighbor, distance) in graph.GetEdges(vertex))
+            {
+                var alt = dist[vertex] + distance;
+                if (alt < dist[neighbor])
+                {
+                    dist[neighbor] = alt;
+                    if (!prev.TryAdd(neighbor, vertex))
+                    {
+                        prev[neighbor] = vertex;
+                    }
+
+                    if (!queue.UnorderedItems.Select(x => x.Element).Contains(neighbor))
+                    {
+                        queue.Enqueue(neighbor, alt);
+                    }
+                }
+            }
+        }
+
+        return new ShortestPathGraphResult<T>(dist, prev);
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="graph"></param>
+    /// <param name="source"></param>
+    /// <param name="target"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static IEnumerable<T> GetShortestPath<T>(Graph<T> graph, T source, T target)
+        where T : IEquatable<T?>
+    {
+        var shortestPathGraph = GetShortestPathGraph(graph, source).PreviousNodes;
+
+        var stack = new Stack<T>();
+        var node = target;
+        stack.Push(node);
+        while (shortestPathGraph.ContainsKey(node) && !node.Equals(source))
+        {
+            stack.Push(shortestPathGraph[node]);
+            node = shortestPathGraph[node];
+        }
+
+        return stack.ToList();
+    }
+}
---
+++ CodeChallenge.Core/Helpers/Math/Graph.cs
@@ -0,0 +1,55 @@
+﻿namespace CodeChallenge.Core.Helpers.Math;
+
+public class Graph<T>
+    where T : IEquatable<T?>
+{
+    public record Edge(T First, T Second, int Distance);
+
+    private readonly List<T> _vertices;
+    private readonly List<Edge> _edges;
+
+    public Graph()
+    {
+        _vertices = new List<T>();
+        _edges = new List<Edge>();
+    }
+
+    public IReadOnlyList<T> GetVertices() => _vertices;
+
+    public IReadOnlyList<T> GetNeighbors(T vertex)
+    {
+        return _edges
+            .Where(edge => edge.First.Equals(vertex))
+            .Select(edge => edge.Second)
+            .ToList();
+    }
+
+    public IReadOnlyList<Edge> GetEdges(T vertex)
+    {
+        return _edges
+            .Where(edge => edge.First.Equals(vertex))
+            .ToList();
+    }
+
+    public void AddVertex(T vertex)
+    {
+        if (!_vertices.Contains(vertex))
+        {
+            _vertices.Add(vertex);
+        }
+    }
+
+    public void AddEdge(T vertex1, T vertex2, int distance = 1)
+    {
+        var edge = new Edge(vertex1, vertex2, distance);
+        if (!_edges.Contains(edge))
+        {
+            _edges.Add(edge);
+        }
+    }
+
+    public void RemoveEdge(T vertex1, T vertex2)
+    {
+        _edges.RemoveAll(edge => edge.First.Equals(vertex1) && edge.Second.Equals(vertex2));
+    }
+}
