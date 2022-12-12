38d44e28b39cc550cbfe0e59b69f0824d6b75ee8
Randy Ehret <randyehret@gmail.com>
2022-12-12 12:25:08 -0600

    Refactor input provider for advent 2022 12

--- Solutions/AdventOfCode/2022/CodeChallenge.AdventOfCode.AdventOfCode2022/Day12/Day12InputProviderBuilderExtensions.cs
+++ Solutions/AdventOfCode/2022/CodeChallenge.AdventOfCode.AdventOfCode2022/Day12/Day12InputProviderBuilderExtensions.cs
@@ -25,57 +25,65 @@ internal static class Day12InputProviderBuilderExtensions
 
     private static HeightmapWithStartAndEnd Parse(IEnumerable<string> lines, DijkstraType type)
     {
-        var coordinates = new List<Coordinate>();
         var start = new Coordinate(0, 0, 0);
         var end = new Coordinate(0, 0, 0);
 
-        var linesArray = lines.ToArray();
-        for (var y = 0; y < linesArray.Length; y++)
+        var coordinateGrid = lines.Select((line, y) =>
         {
-            var line = linesArray[y].ToCharArray();
-            for (var x = 0; x < line.Length; x++)
-            {
-                var @char = line[x];
-                switch (@char)
+            return line.ToCharArray()
+                .Select((@char, x) =>
                 {
-                    case 'S':
-                        start = new Coordinate(x, y, 0);
-                        coordinates.Add(start);
-                        break;
-                    case 'E':
-                        end = new Coordinate(x, y, 'z' - 'a');
-                        coordinates.Add(end);
-                        break;
-                    default:
-                        coordinates.Add(new Coordinate(x, y, @char - 'a'));
-                        break;
-                }
-            }
-        }
+                    switch (@char)
+                    {
+                        case 'S':
+                            start = new Coordinate(x, y, 0);
+                            return start;
+                        case 'E':
+                            end = new Coordinate(x, y, 'z' - 'a');
+                            return end;
+                        default:
+                            return new Coordinate(x, y, @char - 'a');
+                    }
+                })
+                .ToArray();
+        }).ToArray();
 
         var graph = new Graph<Coordinate>();
-        foreach (var coordinate in coordinates)
+        for (var y = 0; y < coordinateGrid.Length; y++)
         {
-            graph.AddVertex(coordinate);
-            foreach (var neighbor in GetNeighbors(coordinates, coordinate, type))
+            for (var x = 0; x < coordinateGrid[y].Length; x++)
             {
-                graph.AddEdge(coordinate, neighbor);
+                var coordinate = coordinateGrid[y][x];
+                graph.AddVertex(coordinate);
+                var neighbors = GetNeighbors(coordinateGrid, x, y, type);
+                foreach (var neighbor in neighbors)
+                {
+                    graph.AddEdge(coordinate, neighbor);
+                }
             }
         }
 
         return new HeightmapWithStartAndEnd(graph, start, end);
     }
 
-    private static IEnumerable<Coordinate>GetNeighbors(IEnumerable<Coordinate> coordinates, Coordinate coordinate, DijkstraType type)
+    private static IEnumerable<Coordinate> GetNeighbors(IReadOnlyList<IReadOnlyList<Coordinate>> coordinates, int x, int y, DijkstraType type)
     {
-        return coordinates
-            .Where(x =>
-                (x.X == coordinate.X - 1 && x.Y == coordinate.Y)
-                || (x.X == coordinate.X + 1 && x.Y == coordinate.Y)
-                || (x.X == coordinate.X && x.Y == coordinate.Y - 1)
-                || (x.X == coordinate.X && x.Y == coordinate.Y + 1))
-            .Where(x => type == DijkstraType.StartIsSource
-                ? x.Height <= coordinate.Height + 1 // When Start is source, we find coordinates whose heights are at most 1 higher than the current coordinate
-                : x.Height >= coordinate.Height - 1); // When End is source, we find coordinates whose heights are at most 1 less than the current coordinate
+        Func<Coordinate, Coordinate, bool> comparer = type == DijkstraType.StartIsSource
+            ? (coord, other) => other.Height <= coord.Height + 1
+            : (coord, other) => other.Height >= coord.Height - 1;
+
+        var neighbors = new List<Coordinate>();
+        var coordinate = coordinates[y][x];
+
+        if (x > 0 && comparer(coordinate, coordinates[y][x - 1]))
+            neighbors.Add(coordinates[y][x - 1]);
+        if (x < coordinates[y].Count - 1 && comparer(coordinate, coordinates[y][x + 1]))
+            neighbors.Add(coordinates[y][x + 1]);
+        if (y > 0 && comparer(coordinate, coordinates[y - 1][x]))
+            neighbors.Add(coordinates[y - 1][x]);
+        if (y < coordinates.Count - 1 && comparer(coordinate, coordinates[y + 1][x]))
+            neighbors.Add(coordinates[y + 1][x]);
+
+        return neighbors;
     }
 }
