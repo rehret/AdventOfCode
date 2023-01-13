c3d80babdfde2434a40d6fb7b3869a5e4547a05f
Randy Ehret <randyehret@gmail.com>
2023-01-13 15:23:12 -0600

    Small refactor to TomtelCoreI69Emulator/Program.cs

--- Solutions/TomsDataOnion/Toys/TomtelCoreI69Emulator/Program.cs
+++ Solutions/TomsDataOnion/Toys/TomtelCoreI69Emulator/Program.cs
@@ -6,21 +6,25 @@ using System.Text;
 using CodeChallenge.TomsDataOnion.Solutions.Layer6.TomtelCoreI69Emulator;
 
 var rootCommand = new RootCommand("TomtelCoreI69Emulator");
-var filePathArgument = new Argument<FileInfo>("FilePath", description: "Specifies the input file containing TomtelCorel69 assembly code");
+var filePathArgument = new Argument<FileInfo>("FilePath", description: "Specifies the input file containing TomtelCoreI69 assembly code");
 rootCommand.AddArgument(filePathArgument);
 rootCommand.SetHandler(async filePath =>
 {
-    var fileStream = filePath.Open(FileMode.Open, FileAccess.Read);
-    await using var fileStreamDisposable = fileStream.ConfigureAwait(false);
+    string program;
 
-    using var streamReader = new StreamReader(fileStream);
-    var program = await streamReader.ReadToEndAsync().ConfigureAwait(false);
+    var fileStream = filePath.Open(FileMode.Open, FileAccess.Read);
+    await using (fileStream.ConfigureAwait(false))
+    {
+        using var streamReader = new StreamReader(fileStream);
+        program = await streamReader.ReadToEndAsync().ConfigureAwait(false);
+    }
 
     var machine = new TomtelCoreI69Emulator();
     await using var machineDisposable = machine.ConfigureAwait(false);
 
     machine.LoadProgram(TomtelCoreI69Emulator.LoadProgramFromString(program));
     var result = machine.Execute();
+
     Console.WriteLine(Encoding.UTF8.GetString(result.ToArray()));
 }, filePathArgument);
 
