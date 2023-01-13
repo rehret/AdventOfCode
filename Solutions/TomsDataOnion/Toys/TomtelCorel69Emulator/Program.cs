c68a74fc1693e2da2a80596e583172063cc27848
Randy Ehret <randyehret@gmail.com>
2023-01-13 14:35:45 -0600

    Add TomtelCorel69Emulator project
    Creates a simple CLI wrapper around the TomtelCorel69 emulator in Tom's Data Onion layer 6

---
+++ Solutions/TomsDataOnion/Toys/TomtelCorel69Emulator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using System.Text;
+
+using CodeChallenge.TomsDataOnion.Solutions.Layer6.TomtelCorel69Emulator;
+
+var rootCommand = new RootCommand("TomtelCore69Emulator");
+var filePathArgument = new Argument<FileInfo>("FilePath", description: "Specifies the input file containing TomtelCorel69 assembly code");
+rootCommand.AddArgument(filePathArgument);
+rootCommand.SetHandler(async filePath =>
+{
+    var fileStream = filePath.Open(FileMode.Open, FileAccess.Read);
+    await using var fileStreamDisposable = fileStream.ConfigureAwait(false);
+
+    using var streamReader = new StreamReader(fileStream);
+    var program = await streamReader.ReadToEndAsync().ConfigureAwait(false);
+
+    var machine = new TomtelCoreI69Emulator();
+    await using var machineDisposable = machine.ConfigureAwait(false);
+
+    machine.LoadProgram(TomtelCoreI69Emulator.LoadProgramFromString(program));
+    var result = machine.Execute();
+    Console.WriteLine(Encoding.UTF8.GetString(result.ToArray()));
+}, filePathArgument);
+
+var parser = new CommandLineBuilder(rootCommand).UseDefaults().Build();
+return await parser.InvokeAsync(args).ConfigureAwait(false);
---
+++ Solutions/TomsDataOnion/Toys/TomtelCorel69Emulator/README.md
@@ -0,0 +1,13 @@
+# TomtelCorel69 Emulator
+
+## Building
+```powershell
+dotnet publish -r <architechture> -c Release
+# where <architecture> is the system to build for, such as win-x64
+```
+
+## Running
+```powershell
+# From within .\bin\Release\<dotnet-version>\<architecture>\publish
+.\TomtelCorel69Emulator.exe <path to input file>
+```
