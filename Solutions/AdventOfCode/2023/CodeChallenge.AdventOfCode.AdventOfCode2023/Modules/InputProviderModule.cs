87f5e3dc5b3bdb390f18193e7c5262931017c156
Randy Ehret <randyehret@gmail.com>
2023-12-13 11:46:28 -0600

    Add Advent of Code 2023 project

---
+++ Solutions/AdventOfCode/2023/CodeChallenge.AdventOfCode.AdventOfCode2023/Modules/InputProviderModule.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeChallenge.AdventOfCode.AdventOfCode2023.Modules;
+
+using System.Reflection;
+
+using CodeChallenge.Core.Modules;
+
+internal class InputProviderModule() : InputProviderAutoRegisteringModule(Assembly.GetExecutingAssembly());
---
+++ Solutions/AdventOfCode/2023/CodeChallenge.AdventOfCode.AdventOfCode2023/Modules/SolutionModule.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeChallenge.AdventOfCode.AdventOfCode2023.Modules;
+
+using System.Reflection;
+
+using CodeChallenge.AdventOfCode.Attributes;
+using CodeChallenge.Core.Modules;
+
+internal class SolutionModule()
+    : SolutionAutoRegisteringModule<AdventOfCodeSolutionAttribute>(Assembly.GetExecutingAssembly());
